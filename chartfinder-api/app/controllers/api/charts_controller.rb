4bb8d5b455836196ab069c07ebc36c29b0eaf3b7
Lindsey Wells <github email address>
2020-08-02 21:56:03 +0100

    add 'pry' to gemfile

--- chartfinder-api/app/controllers/api/charts_controller.rb
+++ chartfinder-api/app/controllers/api/charts_controller.rb
@@ -5,6 +5,10 @@ before_action :set_chart, only: [:show, :edit, :destroy]
     render json: Chart.all
   end
 
+  def show
+    render json: @chart
+  end
+
   def create
     chart=Chart.new(chart_params)
     if chart.save
@@ -14,8 +18,13 @@ before_action :set_chart, only: [:show, :edit, :destroy]
     end
   end
 
-
-
+  def destroy 
+    if @chart.destroy
+      render status:204
+    else
+      render json:{message: "Unable to remove this chart"}, status: 400
+    end
+  end
 
     private
 
