afdc511e9685b68881a3aa1b78a50dbb82eee374
Lindsey Wells <github email address>
2020-08-08 11:02:17 +0100

    adjust chart.css

--- chartfinder-client/src/containers/Chart.js
+++ chartfinder-client/src/containers/Chart.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Chart.css'
+import Song from './Song'
 
 
 
@@ -10,8 +11,7 @@ const Chart = (props) =>(
     <h3>{props.chart.date}</h3>
     <p>{props.chart.country}</p>
       <div className="SongsContainer">
-      {props.chart.songs.map(song=> <div className="SongCard" key={song.id}><p>{song.name} 
-      - {song.artist}</p> <img className="SongImage" src={song.img_url} alt={song.name}></img></div>)}
+      {props.chart.songs.map(song=> <Song song={song} /> )}
     </div>
   </div>
 </div>)
---
+++ chartfinder-client/src/containers/Song.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Chart.css'
+
+
+const Song = (props) =>
+(<div className="SongCard" key={props.song.id}><p>{props.song.name}  - 
+{props.song.artist}</p> <img className="SongImage" src={props.song.img_url} alt={props.song.name}></img></div>)
+
+export default Song
