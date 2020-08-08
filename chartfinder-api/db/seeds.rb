15ede1f1015e95388e3da2999748f73522ed2ac4
Lindsey Wells <github email address>
2020-08-08 10:29:37 +0100

    change seeds to better reflect charts

--- chartfinder-api/db/seeds.rb
+++ chartfinder-api/db/seeds.rb
@@ -10,9 +10,12 @@ song_a = Song.create(name: "Do Be Do", artist: "S Club 7", charts: [], img_url:"
 song_b = Song.create(name: "Best Song Ever", artist: "Kylie Minogue", charts: [], img_url:"https://vignette.wikia.nocookie.net/kylieminogue/images/5/52/2_Hearts.jpg/revision/latest/scale-to-width-down/220?cb=20161113160538")
 song_c = Song.create(name: "MMMM Bwop", artist: "Take That", charts: [], img_url:"https://upload.wikimedia.org/wikipedia/en/a/a7/Take_that_and_party.jpg")
 song_d = Song.create(name: "BananaFarmer", artist: "Vengaboys", charts: [], img_url:"https://upload.wikimedia.org/wikipedia/en/f/f8/BoomBoomBoomBoom.jpg")
+song_e = Song.create(name: "SongE", artist: "E's are good", charts: [], img_url:"https://i.ytimg.com/vi/9bQzbduuGlQ/maxresdefault.jpg")
 
-chart_a = Chart.create(date: "08-01-1984",  country:"UK", songs: [song_d, song_b])
-chart_b = Chart.create(date: "11-08-1989",  country:"Japan", songs:[song_a, song_c] )
-chart_c = Chart.create(date: "25-12-1995",  country:"USA", songs: [song_c, song_b])
+
+
+chart_a = Chart.create(date: "08-01-1984",  country:"UK", songs: [song_a, song_b, song_c, song_d])
+chart_b = Chart.create(date: "11-08-1989",  country:"Japan", songs:[song_e] )
+
 
 
