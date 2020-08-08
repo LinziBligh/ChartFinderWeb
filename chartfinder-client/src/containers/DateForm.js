651fe7a079dad5c6acd230008687376fceb364a2
Lindsey Wells <github email address>
2020-08-08 11:54:48 +0100

    add date picker to form

--- chartfinder-client/src/containers/DateForm.js
+++ chartfinder-client/src/containers/DateForm.js
@@ -5,17 +5,26 @@ class DateForm extends Component {
   constructor(props){
     super(props)
     this.state={
-      date: ""
+      date: "9/9/99"
     }
   }
 
+  handleOnChange = event => {
+    this.setState({
+      date: event.target.value
+    })
+  }
 
   render (){
     return(
       
       <div>
         Pick A Date
-        <p>(this.state.date)</p>
+        <p>{this.state.date}</p>
+        <form>
+          <input type="date" onChange={event=>this.handleOnChange(event)} value={this.state.date}/>
+        <input type="submit" />
+        </form>
        </div>)
   }
 
