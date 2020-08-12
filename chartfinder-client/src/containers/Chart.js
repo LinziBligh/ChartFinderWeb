27765f8671157e91dfa854a5756a2da80c501b3d
Lindsey Wells <github email address>
2020-08-12 14:03:26 +0100

    change chart.js to be a component

--- chartfinder-client/src/containers/Chart.js
+++ chartfinder-client/src/containers/Chart.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './Chart.css'
 import SongCard from '../components/SongCard'
 import DateForm from './DateForm'
 
 
+class Chart extends Component{
+  
+  render(){
+    return(
+      <div className="ChartsContainer">
+        <h1>ChartFinder</h1>
+        <DateForm submitDateForm={this.props.submitDateForm}/>
+        <h3>{this.props.chart.date}</h3>
+        <p>{this.props.chart.country}</p>
+      
+        <div>
+          {this.props.chart.songs.map(song=> <SongCard song={song} /> )}
+        </div>
+        </div>
+      )
 
-const Chart = (props) =>(
-<div className="ChartsContainer">
-  <h1>ChartFinder</h1>
-  <DateForm submitDateForm={props.submitDateForm}/>
-  <h3>{props.chart.date}</h3>
-  <p>{props.chart.country}</p>
-
-  <div>
-    {props.chart.songs.map(song=> <SongCard song={song} /> )}
-  </div>
-  </div>
-)
+  
+  
+  }
+}
 
 export default Chart
 
