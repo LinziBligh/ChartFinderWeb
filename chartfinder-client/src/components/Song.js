7af5a456da8bdc8e7fb33c30c98bc999ba40e165
Lindsey Wells <github email address>
2020-08-13 21:31:19 +0100

    move spotify to a component

--- chartfinder-client/src/components/Song.js
+++ chartfinder-client/src/components/Song.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Song = (props) =>
 (
-<div className="SongCard" key={props.song.id}>
+<div  key={props.song.id}>
 
 <h1>{props.song.position}</h1>
 <p>{props.song.name} - 
@@ -11,6 +11,5 @@ const Song = (props) =>
 
 <img className="SongImage" src={props.song.img_url} alt={props.song.name}></img>
 
-{<iframe className="player-spotify" title={`${props.song.name}`} src={`https://embed.spotify.com/?uri=spotify:track:${props.song.spotify_id}`} width="300" height="80" frameBorder="0" allowtransparency="true"></iframe>}
 </div>)
 export default Song
--- chartfinder-client/src/components/SongCard.js
+++ chartfinder-client/src/components/SongCard.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Song from './Song'
+import Spotify from './Spotify'
 
-const SongCard = ({song}) => (
- 
-<Song song={song} key={song.id}/>
-)
+class SongCard extends Component {
+  render(){
+    return(
+      <div className="SongCard">
+    <Song song={this.props.song} key={this.props.song.id} onMouseEnter={console.log('in!!')} onMouseLeave={console.log('out!!')}/>
+    <Spotify song={this.props.song}/>
+    </div>)
+  }
 
+}
 export default SongCard
+
 
