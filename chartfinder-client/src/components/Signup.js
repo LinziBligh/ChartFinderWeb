98d73e1773fcb8670b32d60530e7117686e6c874
Lindsey Wells <github email address>
2020-08-21 21:22:32 +0100

    add basic signup form

--- chartfinder-client/src/components/Signup.js
+++ chartfinder-client/src/components/Signup.js
@@ -4,7 +4,16 @@ class Signup extends Component {
   render() {
     return (
       <div>
-        <h3>I'm a signup form</h3>
+        <h1>Signup here to save your playlists!</h1>
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <label>Username</label>
+          <input type="text" />
+          <br />
+          <label>Password</label>
+          <input type="password" />
+          <br />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
