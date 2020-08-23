7ab59b8a45152919a1de8fec695c5ade89b5fd60
Lindsey Wells <github email address>
2020-08-23 16:24:14 +0100

    create controlled login and signup forms

--- chartfinder-client/src/components/Login.js
+++ chartfinder-client/src/components/Login.js
@@ -1,7 +1,30 @@
 import React, { Component } from "react";
 
 class Login extends Component {
+
+  constructor(){
+    super();
+    this.state={
+      username: "",
+      password: ""
+    }
+  }
+
+handleNameChange = (event)=>{
+ this.setState({
+   username : event.target.value
+ })
+}
+
+handlePasswordChange = (event)=>{
+  this.setState({
+    password : event.target.value
+  })
+ }
+
+
   handleSubmit = (event) => {
+    event.preventDefault()
     alert("ive been clicked");
   };
 
@@ -11,10 +34,10 @@ class Login extends Component {
         <h1>Login here to view your saved playlists!</h1>
         <form onSubmit={(event) => this.handleSubmit(event)}>
           <label>Username</label>
-          <input type="text" />
+          <input type="text" onChange={(event)=>this.handleNameChange(event)} value={this.state.username}/>
           <br />
           <label>Password</label>
-          <input type="password" />
+          <input type="password"  onChange={(event)=>this.handlePasswordChange(event)} value={this.state.password} />
           <br />
           <input type="submit" />
         </form>
