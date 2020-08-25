f12c082f6fc833c1795f99a64b7b1253a763acda
Lindsey Wells <github email address>
2020-08-25 06:03:01 +0100

    fix security issue with serialize-javascript

--- chartfinder-client/src/index.js
+++ chartfinder-client/src/index.js
@@ -1,40 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux'
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import store from './store.js'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import store from "./store.js";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import App from "./containers/App";
-//import history from "./history";
-
-
-
-//import Routes from './Routes';
 
 
 ReactDOM.render(
-  <Provider store={store} >
-  <BrowserRouter >
-  
-    
-    <Switch>
-          <Route exact path="/" component={App} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/login" component={Login} />
-        </Switch>
-    
-  
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
-
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
