64e022053a188d16791d47daccb0575d4f5d8fa2
Lindsey Wells <github email address>
2020-08-27 15:32:00 +0100

    fix onclick for dashboard from autoclick

--- chartfinder-client/src/containers/Dashboard.js
+++ chartfinder-client/src/containers/Dashboard.js
@@ -1,25 +1,36 @@
-
 import { connect } from "react-redux";
 import React, { Component } from "react";
+import { fetchChart } from "../actions/fetchChart";
+import { fetchBirthdayChart } from "../actions/fetchBirthdayChart";
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
         <p>Your Saved Playlists</p>
-       {this.props.user_charts.map(chart=><li>{chart.date}</li>)}
+        {this.props.user_charts ? this.props.user_charts.map((chart) => (<button onClick={()=>this.props.submitDate(chart.date.split("-").reverse().join("-"))}>
+            {chart.date} - Daily Chart</button>)) : <p>no charts to show</p>}
+
+            {this.props.user_birthdays ? this.props.user_birthdays.map((chart) => (<button onClick={()=>this.props.submitBirthday(chart.date.split("-").reverse().join("-"))}>
+            {chart.date} - Birthday Chart</button>)) : <p>no birthdays to show</p>}
       </div>
     );
   }
 }
 
-const mapStateToProps = state =>{
-  return{
-   user: state.charts.user,
-   user_charts: state.charts.user.charts,
-   user_birthdays: state.charts.user.charts
-  }
-}
+const mapStateToProps = (state) => {
+  return {
+    user: state.charts.user,
+    user_charts: state.charts.user.charts,
+    user_birthdays: state.charts.user.birthdays,
+  };
+};
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    submitDate: (date) => dispatch(fetchChart(date)),
+    submitBirthday: (date) => dispatch(fetchBirthdayChart(date)),
+  };
+};
 
-export default connect(mapStateToProps)(Dashboard)
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
