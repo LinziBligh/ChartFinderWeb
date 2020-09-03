f3a05e22d81bfb4075d61126e6c2df7f7d04daf2
Lindsey Wells <github email address>
2020-09-03 22:00:48 +0100

    add styling to popup

--- chartfinder-client/src/components/Modal.js
+++ chartfinder-client/src/components/Modal.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Popup from "reactjs-popup"
 
   const Modal = (props) => (
-    <Popup
+    <Popup 
       trigger={<button className="button"> Export Playlist To Spotify </button>}
       modal
       closeOnDocumentClick
     >
+      <div className="popup-content">
       <h2>Spotify Playlist</h2>
       <h3>Click these links to open up the full tracks in Spotify, and add them to a new playlist!</h3>
       <ul > {props.chart.songs.map((song) => (
@@ -14,6 +15,7 @@ import Popup from "reactjs-popup"
           {<a href={`spotify:track:${song.spotify_id}`}>{song.name}</a>}
         </li>))}
         </ul>
+        </div>
       
     </Popup>
   );
