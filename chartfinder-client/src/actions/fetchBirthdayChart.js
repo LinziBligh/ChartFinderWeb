3cde01a08eafb561d7cf613dd3ef522a44932c27
Lindsey Wells <github email address>
2020-09-04 11:18:42 +0100

    persist state after refresh if no user logged in

--- chartfinder-client/src/actions/fetchBirthdayChart.js
+++ chartfinder-client/src/actions/fetchBirthdayChart.js
@@ -1,8 +1,12 @@
+import store from "../store"
+
 export function fetchBirthdayChart(date) {
   return (dispatch) => {
     dispatch({ type: "START_FETCHING_BIRTHDAY_REQUEST" });
     fetch(`http://localhost:3001/api/birthdays/${date}`)
       .then((response) => response.json())
-      .then((chart) => dispatch({ type: "ADD_BIRTHDAY", chart }));
+      .then((chart) => {dispatch({ type: "ADD_BIRTHDAY", chart })
+      localStorage.setItem("state", JSON.stringify(store.getState()))});
+
   };
 }
--- chartfinder-client/src/actions/fetchChart.js
+++ chartfinder-client/src/actions/fetchChart.js
@@ -1,9 +1,12 @@
+import store from "../store"
+
 export function fetchChart(date){
   return (dispatch) => {
     dispatch({type:'START_FETCHING_CHART_REQUEST'});
     fetch(`http://localhost:3001/api/charts/${date}`)
     .then(response => response.json())
-    .then (chart=> dispatch({type: "ADD_CHART", chart})
+    .then (chart=> {dispatch({type: "ADD_CHART", chart});
+    localStorage.setItem("state", JSON.stringify(store.getState()))}
     )
   }
   }
