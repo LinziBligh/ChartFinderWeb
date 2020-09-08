bae869a60ff4bdf130b482b38b452a099ded73f8
Lindsey Wells <github email address>
2020-09-08 10:14:42 +0100

    tidy birthdays_controller

--- chartfinder-api/app/controllers/api/birthdays_controller.rb
+++ chartfinder-api/app/controllers/api/birthdays_controller.rb
@@ -1,55 +1,28 @@
 require 'open-uri'
 
 class Api::BirthdaysController<ApplicationController
-before_action :set_birthday, only: [:show, :edit, :destroy]
-
-  
-#route not being used
-def index
-    birthdays=Birthday.all
-    render json: ChartSerializer.new(birthdays).to_serialized_json 
-  end
-#using same serialiser, maybe could rename to something generic?
-
+before_action :set_birthday, only: [:show]
  
 def show
-    if @birthday
+    if @birthday #if it already exists in the database, render it as JSON
      render json: ChartSerializer.new(@birthday).to_serialized_json
-    else
+    else # otherwise make a new Birthday instance
       new_birthday = Birthday.create(date: @date, country: "UK")
 
       birthday_dates(@date).each{|date|
-        date_reversed=date.split("-").reverse.join("-")
-        found_chart=Chart.where('DATE(?) BETWEEN start_date AND end_date', date_reversed).first
-        if found_chart 
-          new_birthday.songs<<found_chart.songs.first
-         else 
-      date_to_url(date)
-      song = Song.create(scrape(@url, date))
-      new_birthday.songs<<song
-      end}
+        date_reversed = date.split("-").reverse.join("-")
+        found_chart = Chart.where('DATE(?) BETWEEN start_date AND end_date', date_reversed).first
+          if found_chart 
+            new_birthday.songs << found_chart.songs.first
+          else 
+            date_to_url(date)
+            song = Song.create(scrape(@url, date))
+            new_birthday.songs << song
+          end
+        }
       new_birthday.save
-    @birthday = Birthday.find_by(date: @date)
+      @birthday = Birthday.find_by(date: @date)
       render json: BirthdaySerializer.new(@birthday).to_serialized_json
-    end
-  end
-
-
-  #not being used at the moment
-  def create
-    birthday=Birthday.new(birthday_params)
-    if chart.save
-      render json: chart
-    else
-      render json:{message: chart.errors}, status: 400
-    end
-  end
-
-  def destroy 
-    if @chart.destroy
-      render status:204
-    else
-      render json:{message: "Unable to remove this chart"}, status: 400
     end
   end
 
@@ -64,10 +37,10 @@ def show
     ##helper method to flip date and set chart
     def set_birthday
       @date = params[:id].split("-").reverse.join("-")
-      @birthday = Birthday.find_by(date: @date)
-      
+      @birthday = Birthday.find_by(date: @date)  
     end
 
+    ##helper method to return each year, from requested year to present year
     def birthday_dates(dob)
       anniversaries=[]
       start_date = dob.split("-")
@@ -93,8 +66,6 @@ def show
       song = doc.css("table.chart-positions div.track")[0]
       year = date.split("-")[2]
 
-      
-
         number_one = {}
         number_one[:name] = song.css(".title a").text.split.map(&:capitalize).join(' ')
         number_one[:artist] = song.css(".artist a").text.split.map(&:capitalize).join(' ')
@@ -103,26 +74,18 @@ def show
         number_one[:position] = 1
         number_one[:year] = year
 
-        
-        
         track = find_spotify_details(number_one[:name])
-    
-       
-        if track.first 
-          number_one[:spotify_id]=track.first.id 
-          number_one[:img_url] = track.first.album.images.first["url"]
-        else
-         number_one[:spotify_id]= ""
-        end
-        number_one
+          if track.first 
+            number_one[:spotify_id]=track.first.id 
+            number_one[:img_url] = track.first.album.images.first["url"]
+          else
+            number_one[:spotify_id]= ""
+          end
+      number_one
     end
 
-    def find_spotify_details(name)
+    def find_spotify_details(name) #might need to improve this as some songs not correct, only searching title
         track = RSpotify::Track.search(name)
     end
-
-
-
-
 
 end
